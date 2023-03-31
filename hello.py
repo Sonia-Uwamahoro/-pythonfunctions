331f6cacd1b6d82ddaef173709c0d03ae4e1c443
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-03-31 15:29:36 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+
+# Functions
+
+# black of code that optionaly accepts some inputs that does the same work
+# and optionally returns and output.
+
+
+def hello(name):
+    print(f"hello{name}")
+
+
+
---
+++ student.py
@@ -0,0 +1,7 @@
+
+
+def year_of_birth(name, age):
+    year = 2023 - age
+    print(f"hello {name}, you were born in {year}")
+
+# a function have an optional returnkeyword which return an output and ends the function.
